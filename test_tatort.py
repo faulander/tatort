1c923410d2f8aa202167c6a7e51d975ba82fc198
Harald Fauland <hf@mediamid.local>
2019-07-09 15:35:59 +0200

    ci adaptions

--- test_tatort.py
+++ test_tatort.py
@@ -1,5 +1,6 @@
 from tatort import splitTatorte, openPage
 
+
 def test_splitTatorte():
     titel, kommissar, stadt, datum = splitTatorte("Rattennest\n(Kasulke (Berlin))\n\n08.10.1972")
     assert titel == "Rattennest"
